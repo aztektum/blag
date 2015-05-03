ae98126cfcd301c3d9991ae7e418cf4d91395317
Michael Ehlert <mikey.ehlert@gmail.com>
2015-05-03 15:58:11 -0700

    define about view, add about.html template

--- app/views.py
+++ app/views.py
@@ -1,9 +1,15 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
-def index():
+def index(name=None):
     user = {'nickname': 'Michael'} # this luser is so fake
     return render_template('index.html',
                             title='Home',
                             user=user)
+
+@app.route('/about')
+def about(name=None):
+    return render_template('about.html',
+                            title='About')
