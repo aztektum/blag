e69301bfeff224f63c8703429c104b0d941a6350
Michael Ehlert <mikey.ehlert@gmail.com>
2015-05-05 12:49:07 -0700

    add config and start a new form

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+# LoginForm class inherits Form from flask.ext.wtf
+# 
+class LoginForm(Form):
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
+
+
