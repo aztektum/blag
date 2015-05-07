00fc4dffe3b35fc41d4f5c5ea63f5a382facc6c0
Michael Ehlert <mikey.ehlert@gmail.com>
2015-05-07 12:39:10 -0700

    add a basic model, script to create our db, ignore the file though, we dont want to commit our db, update init script and config to support our db

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app) # what is this? why do we still import app and not db
 
-from app import views
+from app import views, models
--- config.py
+++ config.py
@@ -4,8 +4,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'password'
