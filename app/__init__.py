00e2eb7df17c7e0bbf29cd8401047f1dd5de1f86
Michael Ehlert <mikey.ehlert@gmail.com>
2015-05-26 15:37:49 -0700

    link db to app, add some login handling bits

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
 
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app) # what is this? why do we still import app and not db
