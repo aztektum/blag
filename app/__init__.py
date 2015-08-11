60e258e2d43e83e497beff606374add7f687de3d
Michael Ehlert <mikey.ehlert@gmail.com>
2015-08-11 14:38:46 -0700

    start config'ing githuboauth

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,14 @@ from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config('GITHUB_CLIENT_ID')
+app.config('GITHUB_CLIENT_SECRET')
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
+github = GitHub(app)
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
 
 from app import views, models
